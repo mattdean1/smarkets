3268d3e5ca8448ed49cf35a410f0ee5f56c1f0f7
mattdean1 <matt.dean.001@gmail.com>
2017-11-06 18:54:07 +0000

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    semi: ['error', 'never'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+  env: {
+    browser: true,
+  },
+}
