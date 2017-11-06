ee6c0564c0e23ba088226b3571e4e10b885fca54
mattdean1 <matt.dean.001@gmail.com>
2017-11-06 21:41:55 +0000

    Fix broken eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   rules: {
     semi: ['error', 'never'],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': ['warn', { aspects: ['invalidHref'] }],
   },
   env: {
     browser: true,
